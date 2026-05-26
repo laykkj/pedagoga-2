50646fc86ca8d1c24de7e0ea2dce2e8f460af7fd
Laysson <layssonwpt@gmail.com>
2026-05-26 20:32:59 -0300

    v7 (page 1 responsive)

---
+++ contact-site/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lidiane pedagogas</title>
+    <link rel="stylesheet" href="contact-style.css">
+</head>
+<body>
+
+<header class="header">
+    <nav class="navbar">
+      <div class="navbar_brand">
+        <h1 class="navbar_logo"><strong>Psicopedagoga</strong></h1>
+      </div>
+
+      <div class="navbar_actions">
+        <div class="navbar_links">
+          <a href=".">sobre mim</a>
+          <a href="" class="">trabalhos</a>
+          <a href="" class="">feedbacks</a>
+          <a href="" class="">contatos</a>
+        </div>
+        <a class="navbar_cta" href="#">Agendar já!</a>
+      </div>
+    </nav>
+  </header>
+
+  <main class="main">
+    <section class="contact_hero"> 
+      <div class="hero_container">
+        <div class="hero_heading">
+        <h1>Venha fazer parte!</h1>
+        <div class="brand-line"></div>
+        <p>Mande sua mensagem agora no meu <span>whatsapp</span></p>
+      </div>
+
+        <div class="">
+
+        </div>
+        
+        <div class="">
+
+        </div>
+      </div>
+    </div>
+    </section>
+  </main>
+
+</body>
+</html>
+
