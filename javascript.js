6c23f167f2604ca2e92bb0af8846561c77a65610
Laysson <layssonwpt@gmail.com>
2026-05-30 21:04:34 -0300

    v8

--- javascript.js
+++ javascript.js
@@ -66,3 +66,125 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     requestAnimationFrame(animation);
   });
 });
+
+const carousel = document.getElementById("carousel");
+const cards = document.querySelectorAll(".feedback-card");
+
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+const dots = document.querySelectorAll("#dots div");
+
+let currentIndex = 0;
+
+function getCardsPerView(){
+
+  if(window.innerWidth <= 768) return 1;
+
+  if(window.innerWidth <= 1400) return 2;
+
+  return 3;
+}
+
+function updateCarousel(){
+
+  const gap = 30;
+const cardWidth =
+cards[0].offsetWidth + gap;
+
+  carousel.style.transform =
+  `translateX(-${currentIndex * cardWidth}px)`;
+
+  dots.forEach(dot =>
+    dot.classList.remove("active")
+  );
+
+  if(dots[currentIndex]){
+    dots[currentIndex].classList.add("active");
+  }
+}
+
+nextBtn.addEventListener("click",()=>{
+
+  const maxIndex =
+  cards.length - getCardsPerView();
+
+  if(currentIndex < maxIndex){
+    currentIndex++;
+  }else{
+    currentIndex = 0;
+  }
+
+  updateCarousel();
+});
+
+prevBtn.addEventListener("click",()=>{
+
+  const maxIndex =
+  cards.length - getCardsPerView();
+
+  if(currentIndex > 0){
+    currentIndex--;
+  }else{
+    currentIndex = maxIndex;
+  }
+
+  updateCarousel();
+});
+
+dots.forEach((dot,index)=>{
+
+  dot.addEventListener("click",()=>{
+
+    const maxIndex =
+    cards.length - getCardsPerView();
+
+    currentIndex =
+    Math.min(index,maxIndex);
+
+    updateCarousel();
+  });
+
+});
+
+window.addEventListener("resize",()=>{
+
+  const maxIndex =
+  cards.length - getCardsPerView();
+
+  if(currentIndex > maxIndex){
+    currentIndex = maxIndex;
+  }
+
+  updateCarousel();
+});
+
+updateCarousel();
+
+document.getElementById("whatsapp-form").addEventListener("submit", function(e){
+
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const telefone = document.getElementById("telefone").value;
+  const assunto = document.getElementById("assunto").value;
+  const mensagem = document.getElementById("mensagem").value;
+
+  const texto = encodeURIComponent(
+`Olá! Meu nome é ${nome}
+
+Telefone: ${telefone}
+
+Assunto: ${assunto}
+
+Mensagem: ${mensagem}`
+  );
+
+  const numero = "5521980993661";
+
+  window.open(
+    `https://wa.me/${numero}?text=${texto}`,
+    "_blank"
+  );
+
+});
